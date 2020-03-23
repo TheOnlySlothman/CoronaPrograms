9aacc0af7e5bee7583b42e8224666e8d826e9196
TheOnlySlothman <lasselm013@gmail.com>
2020-03-23 13:37:40 +0100

    Worked on edabit

---
+++ Edabit/Edabit/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int[] ArrayOfMultiples(int num, int length)
+        {
+            /*
+             * Array of Multiples
+             * Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num up to length.
+             */
+
+            IEnumerable<int> l = Enumerable.Range(1, length).Select(x => num * x);
+
+            return l.ToArray();
+        }
+
+        public static bool IsAnagram(string str1, string str2)
+        {
+            /*
+             * Check for Anagrams
+             * Create a function that takes two strings and returns either true or false depending on whether they're anagrams or not.
+             */
+
+            List<char> charr1 = str1.ToLower().ToList();
+            List<char> charr2 = str2.ToLower().ToList();
+
+            charr1.Sort();
+            charr2.Sort();
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (charr1[i] != charr2[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
