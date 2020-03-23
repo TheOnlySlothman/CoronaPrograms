d41dc929bcaf2bafbdde9d9c09ffa6b59020974f
TheOnlySlothman <lasselm013@gmail.com>
2020-03-23 09:15:06 +0100

    Game: added menu & option to see highscores

--- Spil-projekt/Spil-projekt/Program.cs
+++ Spil-projekt/Spil-projekt/Program.cs
@@ -10,30 +10,56 @@ namespace Spil_projekt
     {
         static void Main(string[] args)
         {
+            Highscores m_sys = Highscores.ScoreReader("..\\..\\Score_Data.xml");
+            ConsoleKeyInfo i;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. new game");
+                Console.WriteLine("2. highscores");
+                i = Console.ReadKey();
+                switch (i.KeyChar)
+                {
+                    case '1':
+                        Game(m_sys);
+                        break;
+                    case '2':
+                        Highscore(m_sys);
+                        break;
+                    default:
+                        break;
+                }
+
+
+            } while (i.Key != ConsoleKey.Escape);
+        }
+
+        static void Game(Highscores sys)
+        {
+            Console.Clear();
             int points = 0;
             int tries = 3;
 
-            Highscores m_sys = Highscores.ScoreReader("..\\..\\Score_Data.xml");
 
             while (NewGame(tries, out int newTries))
             {
                 points++;
                 tries = newTries;
             }
-            EndGame(points, m_sys);
+            EndGame(points, sys);
         }
 
         static bool NewGame(int tries, out int newTries)
         {
-            int number = new Random().Next(0,11);
+            int number = new Random().Next(0, 11);
             Console.Clear();
             Console.WriteLine("I am thinking of a number between 0 and 10. ");
             Console.WriteLine($"you have {tries} left");
             int guess;
-            
+
             for (int i = tries; i > 0; i--)
             {
-            int.TryParse(Console.ReadLine(), out guess);
+                int.TryParse(Console.ReadLine(), out guess);
                 if (guess < number)
                 {
                     Console.WriteLine("too low");
@@ -70,6 +96,11 @@ namespace Spil_projekt
             sys.Scores.Add(score);
             sys.Save();
 
+            Highscore(sys);
+        }
+
+        static void Highscore(Highscores sys)
+        {
             IEnumerable<Score> query = sys.Scores.OrderBy(order => order.Points).Reverse();
 
             Console.Clear();
