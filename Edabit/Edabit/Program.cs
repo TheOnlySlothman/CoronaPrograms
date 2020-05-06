51f5cd5903caad307891a084cfe527faa38b377b
TheOnlySlothman <lasselm013@gmail.com>
2020-05-06 15:04:56 +0200

    worked on edabit

--- Edabit/Edabit/Program.cs
+++ Edabit/Edabit/Program.cs
@@ -474,7 +474,19 @@ namespace Edabit
                     return num;
                 num++;
             }
+
         }
+            public static string FormatPhoneNumber(int[] numbers)
+            {
+            /*
+             * Phone Number Formatting
+             * 
+             * Create a function that takes an array of 10 numbers (between 0 and 9) and returns a string of those numbers formatted as a phone number (e.g. (555) 555-5555).
+             */
+                string phoneNums = String.Format("({0}{1}{2}) {3}{4}{5}-{6}{7}{8}{9}", numbers[0], numbers[1], numbers[2], numbers[3], numbers[4], numbers[5], numbers[6],
+                          numbers[7], numbers[8], numbers[9]);
+                return phoneNums;
+            }
 
     }
 
