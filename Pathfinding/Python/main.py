5ddd997f5877d0b6753ff86d23f6d8a26d3d0c5b
TheOnlySlothman <lasselm013@gmail.com>
2020-11-03 15:11:24 +0100

    Worked on nodes

--- Pathfinding/Python/main.py
+++ Pathfinding/Python/main.py
@@ -3,9 +3,25 @@ from mazes import Maze
 from pathlib import Path
 
 
+def draw_nodes(position_list, img):
+    img = img.convert('RGB')
+    img_pixels = img.load()
+
+    for x in position_list:
+        img_pixels[x[0], x[1]] = (0, 0, 200)
+
+    img.save('node_map.png')
+
+
 def solve(input_file):
-    im = Image.open(input_file)
-    maze = Maze(im)
+    img = Image.open(input_file)
+    maze = Maze(img)
+
+    for x in maze.node_char_list:
+        print(x)
+
+    draw_nodes([x.Position for x in maze.node_list], img)
 
 
 solve(Path('tiny.png'))
+# solve(Path('braid200.png'))
