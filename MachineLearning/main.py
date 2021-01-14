e2609a9faf6460c9e707d74f76d689b72e62bfbf
TheOnlySlothman <lasselm013@gmail.com>
2021-01-14 15:00:54 +0100

    tested multiplication

--- MachineLearning/main.py
+++ MachineLearning/main.py
@@ -2,55 +2,87 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
-from sklearn import linear_model
+from sklearn import linear_model, datasets, svm
 import numpy as np
 
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('test2.csv')
 
-x = df['year'].values.reshape(-1, 1)
-y = df['data_val'].values
+def linear_regression():
+    df = pd.read_csv('test2.csv')
 
-# for i in range(x.size):
+    x = df['year'].values.reshape(-1, 1)
+    y = df['data_val'].values
 
-"""
-df = pd.read_csv('test.csv')
+    # for i in range(x.size):
 
-x = df.drop(columns=['z'])
-y = df['z']
-"""
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    """
+    df = pd.read_csv('test.csv')
+    
+    x = df.drop(columns=['z'])
+    y = df['z']
+    """
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-"""
-model = DecisionTreeClassifier()
-model.fit(x_train, y_train)
-predictions = model.predict(x_test)
-"""
+    """
+    model = DecisionTreeClassifier()
+    model.fit(x_train, y_train)
+    predictions = model.predict(x_test)
+    """
 
-regr = linear_model.LinearRegression()
+    regr = linear_model.LinearRegression()
 
-regr.fit(x_train, y_train)
-predictions = regr.predict(x_test)
+    regr.fit(x_train, y_train)
+    predictions = regr.predict(x_test)
 
-# score = accuracy_score(y_test, predictions)
-print(x_test)
-print(y_test)
-print(predictions)
-"""
-score = accuracy_score(y_test, predictions)
-print(score)
-"""
-print('Coefficients: \n', regr.coef_)
-print('Mean squared error: %.2f'
-      % mean_squared_error(y_test, predictions))
-print('Coefficient of determination: %.2f'
-      % r2_score(y_test, predictions))
+    # score = accuracy_score(y_test, predictions)
+    print(x_test)
+    print(y_test)
+    print(predictions)
+    """
+    score = accuracy_score(y_test, predictions)
+    print(score)
+    """
+    print('Coefficients: \n', regr.coef_)
+    print('Mean squared error: %.2f'
+          % mean_squared_error(y_test, predictions))
+    print('Coefficient of determination: %.2f'
+          % r2_score(y_test, predictions))
 
-plt.scatter(x_test, y_test,  color='black')
-plt.plot(x_test, predictions, color='blue', linewidth=3)
+    plt.scatter(x_test, y_test, color='black')
+    plt.scatter(x_train, y_train, color='green')
+    plt.plot(x_test, predictions, color='blue', linewidth=5)
 
-plt.xticks(())
-plt.yticks(())
+    plt.xticks(())
+    plt.yticks(())
 
-plt.show()
+    plt.show()
+
+
+def Digit():
+    digits = datasets.load_digits()
+    clf = svm.SVC(gamma=0.001, C=100.)
+
+    x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.2)
+    clf.fit(x_train, y_train)
+    predictions = clf.predict(x_test)
+    score = accuracy_score(y_test, predictions)
+    print(score)
+
+def Multiplication():
+    df = pd.read_csv('test.csv')
+    x = df.drop(columns=['z'])
+    y = df['z']
+
+    clf = svm.SVC(gamma=0.001, C=100.)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    clf.fit(x_train, y_train)
+    predictions = clf.predict(x_test)
+    print(x_test.values)
+    print(y_test.values)
+    print(predictions)
+    score = accuracy_score(y_test.values, predictions)
+    print(score)
+
+Multiplication()
