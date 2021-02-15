18a16b6c6549d93be77abcffe061a0f7374d7145
TheOnlySlothman <59558971+TheOnlySlothman@users.noreply.github.com>
2021-02-15 14:17:41 +0100

    Add files via upload

--- SQL Server Management Studio/create tables.sql
+++ SQL Server Management Studio/create tables.sql
@@ -1,9 +1,13 @@
+drop table Items
+drop table ITypes
+drop table Players
+drop table Classes
 drop table Person
 drop table Company
 drop table OccupationTypes
 drop table Genders
 drop table CTypes
-
+ 
 create table CTypes (
 	Id int not null primary key identity(1,1),
 	Name varchar(15)
@@ -33,4 +37,27 @@ create table Person (
 	Gender bit foreign key references Genders(Id),
 	OccupationId int foreign key references OccupationTypes(Id),
 	CompanyId int foreign key references Company(Id)
+	)
+
+create table Classes (
+	Id int not null primary key,
+	Name varchar(15)
+	)
+
+create table Players (
+	Id int not null primary key,
+	Name varchar(15),
+	ClassId int foreign key references Classes(Id)
+	)
+
+create table ITypes (
+	Id int not null primary key,
+	Name varchar(15)
+	)
+
+create  table Items (
+	Id int not null primary key identity(1,1),
+	Name varchar(15),
+	TypeId int foreign key references ITypes(Id),
+	Weight Decimal
 	)
