164b721009246492df42bf78b44a5846ac84433e
TheOnlySlothman <59558971+TheOnlySlothman@users.noreply.github.com>
2021-02-17 15:02:30 +0100

    Add files via upload

---
+++ SQL Server Management Studio/Show Players.sql
@@ -0,0 +1,7 @@
+use testDB
+
+select Players.Id, Players.Name, Classes.Name as class, Items.Name as item1
+from Players
+inner join Classes on Players.ClassId = Classes.Id
+inner join Inventory on Inventory.Id = Players.Id
+left join Items on Inventory.Item1 = Items.Id
